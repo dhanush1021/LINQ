5ded45bc3b8ca615bb76ae2d54d9096119982825
Dhanush <dhanushdybala10@gmail.com>
2024-02-08 20:57:36 +0530

    [Dhanush] Add : numbers which produce the remainder 0 after divided by 2

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
+            list.Add(2);
+            list.Add(5);
+            list.Add(1);
+            list.Add(8);
+            list.Add(4);
+            list.Add(3);
+            list.Add(6);
+            list.Add(0);
+            list.Add(7);
+            var group = from l in list
+                        where l % 2 == 0
+                        select l;
+            foreach(var l in group)
+                Console.WriteLine(l);
         }
     }
 }
