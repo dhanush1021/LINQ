4399180941e845943f9655faf9e81cb4f34e097d
Dhanush <dhanushdybala10@gmail.com>
2024-02-09 10:16:28 +0530

    [Dhanush] Update : find the +ve numbers from a list of numbers using two where conditions in LINQ Query

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -13,16 +13,16 @@ namespace ConsoleApp3
             List<int> list = new List<int>();
             list.Add(2);
             list.Add(5);
-            list.Add(1);
+            list.Add(-1);
             list.Add(8);
-            list.Add(4);
+            list.Add(-4);
             list.Add(3);
-            list.Add(6);
+            list.Add(-6);
             list.Add(0);
-            list.Add(7);
+            list.Add(-7);
             var group = from l in list
                         orderby l
-                        where l % 2 == 0
+                        where l>=0
                         select l;
             foreach(var l in group)
                 Console.WriteLine(l);
