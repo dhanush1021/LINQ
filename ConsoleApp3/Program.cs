628f4fde0c20a7815691100c6d66f245230709c7
Dhanush <dhanushdybala10@gmail.com>
2024-02-09 14:30:45 +0530

    [Dhanush] Update : display the characters and frequency of each character in a given string

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,17 +10,8 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>();
-            list.Add(2);
-            list.Add(5);
-            list.Add(-1);
-            list.Add(8);
-            list.Add(-4);
-            list.Add(2);
-            list.Add(-6);
-            list.Add(0);
-            list.Add(-7);
-            var group = from l in list
+            string str = "Hello World";
+            var group = from l in str
                         orderby l
                         group l by l into y
                         select y;
