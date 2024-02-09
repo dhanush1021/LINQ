7b9f4975b555847bce9eb911341f6f651947d89f
Dhanush <dhanushdybala10@gmail.com>
2024-02-09 14:27:00 +0530

    [Dhanush] Update : display the number and frequency of a given number from an array

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -16,16 +16,16 @@ namespace ConsoleApp3
             list.Add(-1);
             list.Add(8);
             list.Add(-4);
-            list.Add(3);
+            list.Add(2);
             list.Add(-6);
             list.Add(0);
             list.Add(-7);
             var group = from l in list
                         orderby l
-                        where l>=0
-                        select l;
-            foreach(var l in group)
-                Console.WriteLine(l);
+                        group l by l into y
+                        select y;
+            foreach (var l in group)
+                Console.WriteLine($"{l.Key} : {l.Count()}");
         }
     }
 }
