ed616909f5a87e110b3a511063717a98bfb2a54d
Dhanush <dhanushdybala10@gmail.com>
2024-02-09 15:41:06 +0530

    [Dhanush] Update : to accept list members through the keyboard and display them more than a specific value

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,9 +10,18 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>() { 55 ,200, 740, 76, 230 ,482, 95 };
+            List<int> list = new List<int>();
+            int a;
+            int number = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < number; i++)
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                list.Add(a);
+            }
+            Console.WriteLine("\nEnter the number above you want to display ?");
+            a = Convert.ToInt32(Console.ReadLine());
             var group = from l in list
-                        where l > 80
+                        where l > a
                         select l;
             foreach (var l in group)
             {
