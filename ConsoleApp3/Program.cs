e6e1306eece0b5d900e1f75afa51bf831854db02
Dhanush <dhanushdybala10@gmail.com>
2024-02-09 15:24:21 +0530

    [Dhanush] Update : find a string that starts and ends with a specific character

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,12 +10,10 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>() { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };
-            var group = from l in list
-                        group l by l into y
-                        let count = y.Count()
-                        let mul = y.Count()*y.Key
-                        select new {y.Key,count,mul};
+            string[] str = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            var group = from l in str
+                        where l.StartsWith("A") && l.EndsWith("M")
+                        select l;
             foreach (var l in group)
             {
                 Console.WriteLine(l);
